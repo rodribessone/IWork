c4ed66562d6840ea491766cef69164c04d0d2547
rodribessone <rodrigo-bessone@hotmail.com>
2026-02-04 18:56:43 +1000

    Fix: Case sensitivity import error in main.jsx

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './Context/AuthContext.jsx';
-import { SocketContextProvider } from './context/SocketContext.jsx';
+import { SocketContextProvider } from './Context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
